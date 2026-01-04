988bce7dd880eb1543f83f827c052898d5d174e7
brullgua <157432278+brullgua@users.noreply.github.com>
2026-01-04 01:39:54 +0000

    build: bootstrap Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
+      home: Scaffold(gity
         body: RootPage()
       )
     );
