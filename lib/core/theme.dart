9d093de06e98957e08d6c9c04f6c6440aac83ce3
brullgua <157432278+brullgua@users.noreply.github.com>
2026-01-06 01:01:09 +0000

    feat: Refactor theme colors and update RootPage styling

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-  static Color azulSecundario = const Color(0xFF004182);
-  static Color azulPrimario = const Color(0xFF2766EC);
+  static Color secondary = const Color(0xFF39D2C0);
+  static Color primary = const Color(0xFF4B39EF);
+  static Color tertiary = const Color(0xFFEE8B60);
+  static Color alternate = const Color(0xFF262d34);
   static Color morado = const Color(0xFF8F34EA);
   static Color grisOscuro = const Color(0xFF161F31);
-  static Color negro = const Color(0xFF0F1729);
+  static Color surface = const Color(0xFF1d2428);
   static Color blanco = const Color(0xFFF8FAFC);
   static Color grisClaro = const Color(0xFF94A3B8);
-  static Color negroOscuro = const Color(0xFF0C0C0B);
+  static Color surfaceContainer = const Color(0xFF14181B);
+  static Color warning = const Color(0xFFf9cf58);
+  static Color error = const Color(0xFFff5963);
+  static Color success = const Color(0xFF249689);
+
   static ThemeData themeData = ThemeData(
-    primaryColor: azulPrimario,
+    primaryColor: primary,
     brightness: Brightness.dark,
     fontFamily: 'Poppins',
     colorScheme: ColorScheme(
       brightness: Brightness.dark, 
-      primary: azulPrimario, 
+      primary: primary, 
       onPrimary: blanco, 
-      primaryContainer: azulSecundario,
+      primaryContainer: alternate,
       onPrimaryContainer: blanco,
       secondary: morado, 
       onSecondary: blanco, 
-      error: Colors.red, 
+      error: error, 
       onError: blanco, 
-      surface: negroOscuro,
-      onSurface: negro,
-      surfaceContainer: grisOscuro,
+      surface: surfaceContainer,
+      onSurface: blanco,
+      surfaceContainer: surfaceContainer,
       secondaryContainer: grisClaro,
       tertiary: morado,
     ),
--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:turnopol/providers/root_provider.dart';
+import 'package:turnopol/core/theme.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
@@ -9,18 +10,33 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<RootProvider>(
       builder: (context, rootProvider, child) {    
-      return const Scaffold(
+      return Scaffold(
         bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: [
-              IconButton(icon: Icon(Icons.home), onPressed: null),
-              IconButton(icon: Icon(Icons.settings), onPressed: null),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(
+              color: ThemeColors.surface,
+              borderRadius: BorderRadiusGeometry.circular(24),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.2),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, -2),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(icon: Icon(Icons.home), onPressed: null),
+                IconButton(icon: Icon(Icons.settings), onPressed: null),
             ],
           )
         ),
-        body: Center(
-          child: Text('Root Page'),
         ),
+        body: Center(
+          child: Text('Root Page', style: TextStyle(color: ThemeColors.blanco)))
       );
     });
   }
